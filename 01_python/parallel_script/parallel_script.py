f6de116397cddf8155431a3dc1fcb11e00e3b3bb
Nikhil-555 <nikhil.pabbithi@bizacuity.com>
2024-10-30 14:03:08 +0530

    final push

--- 01_python/parallel_script/parallel_script.py
+++ 01_python/parallel_script/parallel_script.py
@@ -1,28 +1,29 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
 
-# # Define the folder where your Python scripts are located
-# scripts_folder = r"C:\Users\nikhil.pabbithi\Downloads\ETL PYTHON"
+# Define the folder where your Python scripts are located
+scripts_folder = r"C:\Users\nikhil.pabbithi\Desktop\etl python training\ETL_nikhil\01_python"
 
-# # Get all Python scripts in the folder
-# python_scripts = [f for f in os.listdir(scripts_folder) if f.endswith('.py')]
+# Get all Python scripts in the folder
+python_scripts = [f for f in os.listdir(scripts_folder) if f.endswith('.py')]
+print(python_scripts)
 
-# # Function to run each script using os.system()
-# def run_script(script):
-#     script_path = os.path.join(scripts_folder, script)
-#     command = f'python "{script_path}"'
-#     result = os.system(command)
-#     return f"Script {script} finished with exit code {result}"
+# Function to run each script using os.system()
+def run_script(script):
+    script_path = os.path.join(scripts_folder, script)
+    command = f'python "{script_path}"'
+    result = os.system(command)
+    return f"Script {script} finished with exit code {result}"
 
-# # Function to run scripts in parallel
-# def run_scripts_in_parallel(scripts):
-#     with ThreadPoolExecutor() as executor:
-#         results = executor.map(run_script, scripts)
-#         for result in results:
-#             print(result)
+# Function to run scripts in parallel
+def run_scripts_in_parallel(scripts):
+    with ThreadPoolExecutor() as executor:
+        results = executor.map(run_script, scripts)
+        for result in results:
+            print(result)
 
-# if __name__ == '__main__':
-#     run_scripts_in_parallel(python_scripts)
+if __name__ == '__main__':
+    run_scripts_in_parallel(python_scripts)
 
 
 
